dff493e72c2d72db1c500e31e006a4415378d001
euvely <102321531+euvely@users.noreply.github.com>
2025-05-07 04:41:16 +0900

    Update index.jsx

--- src/components/section-experience/index.jsx
+++ src/components/section-experience/index.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import Section from '../section';
 import SummaryItem from '../summary-item';
 
-const SectionExperience = ({ experience }) => {
-  if (!experience.length) return null;
+const SectionEducation = ({ education }) => {
+  if (!education.length) return null;
 
   return (
     <Section title="Experience">
-      {experience.map((item) => (
+      {education.map((item) => (
         <SummaryItem
           key={item.name}
           name={item.name}
@@ -20,4 +20,4 @@ const SectionExperience = ({ experience }) => {
   );
 };
 
-export default SectionExperience;
+export default SectionEducation;
